355dcf57aded182f8981e30b6b07d843c2ad2030
Lidet s <luk4ls@yahoo.com>
2019-10-03 13:48:12 -0700

    methods added

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # movies
+
+##  Movie Dababase Search Engine
+* searching for a movie, 
+* searching for an actor
+* searching through movie genre's
+* Now playing near me
--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express =require("express")
 
 const App=express()
 
+App.use(express.static("public"))
+
 
 port=process.env.port|| 2000
-
-
 
 App.listen(port, function(){
  console.log("app is lisning ")
